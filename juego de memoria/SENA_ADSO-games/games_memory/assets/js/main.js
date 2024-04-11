afb91bda0bb3d52cab598dfa6d3902085316255b
soliyen <124852329+soliyen@users.noreply.github.com>
2024-04-10 21:53:02 -0500

    Juego Memoria

---
+++ juego de memoria/SENA_ADSO-games/games_memory/assets/js/main.js
@@ -0,0 +1,16 @@
+var objClass;
+const contId="containerGame";
+const progress="progressbarId";
+const chronometer="chronometerId";
+const speed=100;
+const maxMilliseconds=2000;
+
+function setLevel(value){
+
+    objClass=new Game(contId,value,progress,chronometer,speed,maxMilliseconds);  
+    
+    
+}
+
+
+
